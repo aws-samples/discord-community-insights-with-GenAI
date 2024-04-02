70b1a0112a4e0a58d31d9fe2360aec73751c3591
dahuahua <38651850@qq.com>
2024-04-02 13:02:03 +0800

    add prompts

---
+++ demo/pages/4_Prompts_List.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import time, requests, json
+
+import pandas as pd
+
+domain_url = st.session_state.domain_url
+api_key = st.session_state.api_key
+
+url = domain_url + "/prompts"
+headers = {
+  'x-api-key': api_key
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+data = json.loads(response.text)
+st.text(data)
+df = pd.json_normalize(data['jobRuns'])
+st.set_page_config(page_title="提示词列表", layout="wide")
+st.title("提示词列表")
+
+df['result_link'] = '/result?job_id=' + df['Id']
+
+st.data_editor(
+    df
+    hide_index=True,
+)
+
+st.markdown("示例代码如下：")
+code = '''
+url = "https://xxxxxx.execute-api.us-east-1.amazonaws.com/prod/jobs"
+
+payload = ""
+headers = {
+  'x-api-key': 'xxxxxxxxxxxxxxxxxxx'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+'''
+st.code(code, language='python')
