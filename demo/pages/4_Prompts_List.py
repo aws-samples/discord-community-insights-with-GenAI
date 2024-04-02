98cd023cac021426af2dfb2c73c6076e4d2a0a7d
dahuahua <38651850@qq.com>
2024-04-02 13:24:23 +0800

    add prompts

--- demo/pages/4_Prompts_List.py
+++ demo/pages/4_Prompts_List.py
@@ -11,29 +11,24 @@ headers = {
   'x-api-key': api_key
 }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request("GET", url, headers=headers)
 data = json.loads(response.text)
-st.text(data)
-df = pd.json_normalize(data['jobRuns'])
-st.set_page_config(page_title="提示词列表", layout="wide")
+df = pd.json_normalize(data)
+
 st.title("提示词列表")
 
-df['result_link'] = '/result?job_id=' + df['Id']
-
 st.data_editor(
-    df
+    df.reindex(columns=['id', 'topic', 'prompt_rag', 'prompt_sentiment']),
     hide_index=True,
 )
 
 st.markdown("示例代码如下：")
 code = '''
-url = "https://xxxxxx.execute-api.us-east-1.amazonaws.com/prod/jobs"
-
-payload = ""
+url = "https://xxxxxx.execute-api.us-east-1.amazonaws.com/prod/prompts"
 headers = {
   'x-api-key': 'xxxxxxxxxxxxxxxxxxx'
 }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request("GET", url, headers=headers)
 '''
 st.code(code, language='python')
---
+++ demo/pages/5_Prompts_Add.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import time, requests, json
+
+domain_url = st.session_state.domain_url
+api_key = st.session_state.api_key
+topic = st.text_input(
+    "Enter Topic👇 (required)",
+    placeholder="input topic name,like '拍卖行'",
+)
+
+prompt_rag = st.text_area(
+    "Enter Prompt Rag👇 (required)",
+    placeholder="Prompt RAG, using to extract relavent information from raw data",
+)
+
+prompt_sentiment = st.text_area(
+    "Enter Prompt Sentiment👇 (required)",
+    placeholder="Prompt Sentiment, using to analysis sentiment infromation from extracted data",
+)
+
+if st.button('提交'):
+    if topic.strip() == "":
+        st.write("Topic cannot be empty！")
+
+    if prompt_rag.strip() == "":
+        st.write("Prompt RAG cannot be empty！")
+
+    if prompt_sentiment.strip() == "":
+        st.write("Prompt sentiment cannot be empty！")
+
+    url = domain_url + "/prompts"
+
+    headers = {
+      'x-api-key': api_key,
+      'Content-Type': 'application/json'
+    }
+    response = json.loads(requests.request("GET", url, headers=headers).text)
+
+    payload = json.dumps({
+      "topic": topic,
+      "prompt_rag": prompt_rag,
+      "prompt_sentiment": prompt_sentiment
+    })
+    headers = {
+      'x-api-key': api_key,
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    st.success('Submit success!', icon="✅")
