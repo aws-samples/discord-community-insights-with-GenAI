9e42e4fad8d3919fd24444352dff21ac1a76e9a1
dahuahua <38651850@qq.com>
2024-04-07 12:16:49 +0800

    add comments for gen env

--- deploy/gen_env.sh
+++ deploy/gen_env.sh
@@ -4,14 +4,21 @@ account_id=`aws sts get-caller-identity --query "Account" --output text`
 ts=`date +%y-%m-%d-%H-%M-%S`
 unique_tag="$account_id-$ts"
 
-
+# Glue 作业名称
 GLUE_JOB_NAME="llm-analysis-text-job"
+# DynamoDB中存储prompt的表名
 LLM_ANALYSIS_TEXT_TABLE_NAME="prompt-template"
-S3_BUCKET_NAME="${unique_tag}-llm-analysis-text"
+# 存储原始数据以及分析结果的S3名称
+S3_BUCKET_NAME="llm-analysis-text-${unique_tag}"
+# S3中原始数据Prefix的名称
 RAW_DATA_PREFIX="raw-data/"
+# Glue Catalog中DB名称
 GLUE_DATABASE="llm_text_db"
+# Glue Catalog中查询S3中分析数据使用表名
 GLUE_TABLE="sentiment_result"
+# Api Gateway 暴露的domain URL，需要在CDK 部署完毕后进行配置
 domain_url="placeholder please change as after the output of cdk deploy result"
+# 调用Api Gateway 暴露的domain URL API key，需要在CDK 部署完毕后进行配置
 apikeys="placeholder"
 echo "GLUE_JOB_NAME=${GLUE_JOB_NAME}" > .env
 echo "LLM_ANALYSIS_TEXT_TABLE_NAME=${LLM_ANALYSIS_TEXT_TABLE_NAME}" >> .env
